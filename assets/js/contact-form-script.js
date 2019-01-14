13ee140c1a8f91f207621af3bcf8e1c2d7d2939f
adir abargil <adir1661@users.noreply.github.com>
2019-01-14 13:47:40 +0200

    version 1

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -7,24 +7,29 @@ $(function () {
     var message = $('#message');
     var last = $('#last');
     var email = $('#email');
+    var phone = $('#phone');
     var name = $('#name');
     $(form).submit(function (event) {
         // Stop the browser from submitting the form.
         event.preventDefault();
         var emailData = email.val();
+        var phoneData = phone.val();
         var lastData = last.val();
         var messageData = message.val();
         var nameData = name.val();
         if (formValidate(email,last,name)) {
+
             db.collection("signs").add({
                 created: firebase.firestore.FieldValue.serverTimestamp(),
                 email: emailData,
                 name: nameData,
                 last: lastData,
-                message: messageData
+                message: messageData,
+                phone:phoneData
             })
                 .then(function (docRef) {
                     console.log("Document written with ID: ", docRef.id);
+                    $(".inform-subscribe").removeClass("hidden-sub");
                 })
                 .catch(function (error) {
                     console.error("Error adding document: ", error);
@@ -34,7 +39,7 @@ $(function () {
 
 
 });
-function formValidate(email,last,name) {
+function formValidate(email,last,name,phone) {
     let flag = true;
     if(!name.val()) {
         informUser("נא למלא שם")
@@ -48,14 +53,29 @@ function formValidate(email,last,name) {
         informUser("נא למלא אימיל אמיתי")
         flag = false;
     }
+    if(!validPhone(phone)){
+        console.log("phone incorrect")
+        flag = false;
+    }
     return flag;
 }
 function validateEmail(email){
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+    if(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+    else return true;
+}
+function validPhone(phone){
+    // var re =/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    // return re.test(phone)
+    return true;
 }
 function informUser(stringFill) {
     // $("#errors").append("<p>"+stringFill+"</p>")
 
 
 }
+
+
+//TODO: rmove links :instagram etc. , add whatsapp link and fb link, fix form to inform user when he signed in , change content accurding to roi
